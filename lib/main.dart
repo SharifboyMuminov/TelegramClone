d2a16b2a509ae3cb6fc1c2a8cfed45ea13f3bbab
Sharifjon <sharifboymuminov@gmail.com>
2024-12-29 22:13:26 +0500

    connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:telegramclone/service/firebase_options.dart';
 
 import 'screens/home_screen/home_screen.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-
 
   runApp(const MyApp());
 }
